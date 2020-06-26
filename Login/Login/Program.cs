1fde3dc5287a9363ae947e21c0ec0a30c059f32c
AuxifyC <67488835+AuxifyC@users.noreply.github.com>
2020-06-27 00:45:27 +0100

    Add files via upload

---
+++ Login/Login/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Net.Configuration;
+using System.Threading;
+
+
+
+namespace Login
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // Initilization
+            Console.Title = "Auxify's MAC Address Spoofer";
+            mTasks MAC = new mTasks();
+
+            // Getting Valid Keys
+            string dKeys = MAC.GetKeys("https://pastebin.com/raw/EX4zuxJS");
+
+            // Getting User's Key
+            Console.Write("Enter Your Key : ", Console.ForegroundColor = ConsoleColor.Blue);
+            string uKey = Console.ReadLine();
+
+            // Checking If User's Key Is Valid
+            if (dKeys.Contains(uKey))
+            {
+                Console.Clear();
+                Console.WriteLine("[!] Key Valid", Console.ForegroundColor = ConsoleColor.Green);
+                Thread.Sleep(1000);
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("[X] Key Invalid",Console.ForegroundColor = ConsoleColor.Red);
+                Thread.Sleep(1000);
+                System.Environment.Exit(0);
+            }
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            string uMAC = MAC.ReadMAC();
+            Console.WriteLine("Current MAC Address : " + uMAC);
+            Thread.Sleep(2000);
+            Console.Write("Do You Want To Change MAC Address ? {Y / N} : ");
+            string ans = Console.ReadLine();
+            Console.Clear();
+            ans = ans.ToLower();
+            if(ans == "y")
+            {
+                Console.Write("Enter Your New MAC Address : ");
+                string nMAC = Console.ReadLine();
+                MAC.ChangeMac(nMAC);
+                Console.Clear();
+                string cMAC = MAC.ReadMAC();
+                Console.WriteLine("Current MAC Address : " + cMAC);
+                Thread.Sleep(3000);
+                System.Environment.Exit(0);
+            }
+            if(ans == "n")
+            {
+                Thread.Sleep(1000);
+                System.Environment.Exit(0);
+            }
+        }
+
+    }
+}
---
+++ Login/Login/mTasks.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Net;
+
+using System.Threading;
+
+namespace Login
+{
+    public class mTasks
+    {
+
+        public string ReadMAC()
+        {
+            RegistryKey rkey;
+            string MAC;
+            try
+            {
+                rkey = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Class\\{4D36E972-E325-11CE-BFC1-08002BE10318}\\0012", true); //--->this is the string to change 
+                MAC = (string)rkey.GetValue("NetworkAddress");
+                rkey.Close();
+                return MAC;
+            }
+            catch
+            {
+                Console.WriteLine("[X] Run As Admin",Console.ForegroundColor = ConsoleColor.Red);
+                Thread.Sleep(2000);
+                System.Environment.Exit(0);
+                return "";
+            }
+        }
+        public string GetKeys(string url)
+        {
+            WebClient wc = new WebClient();
+            try
+            {
+                string dKeys = wc.DownloadString(url);
+                return dKeys;
+            }
+            catch
+            {
+                Thread.Sleep(1000);
+                System.Environment.Exit(0);
+                return "";
+
+            }
+        }
+        public void ChangeMac(string newMAC)
+        {
+            RegistryKey rkey;
+            try
+            {
+                rkey = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Class\\{4D36E972-E325-11CE-BFC1-08002BE10318}\\0012", true);
+                rkey.SetValue("NetworkAddress", newMAC);
+                rkey.Close();
+            }
+            catch
+            {
+                Console.WriteLine("[X] Run As Admin", Console.ForegroundColor = ConsoleColor.Red);
+                Thread.Sleep(2000);
+                System.Environment.Exit(0);
+            }
+        }
+    }
+}
